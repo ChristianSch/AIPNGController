066603f1a962d75d01369575fe71c48f33257487
Christian Schulze <christian.schulze@mni.thm.de>
2014-03-01 18:41:36 +0100

    initial commit

---
+++ AIPNGController.h
@@ -0,0 +1,58 @@
+//
+//  AIPNGController.h
+//  AIPNGController
+//
+//  Created by Christian Schulze on 01.03.14.
+//  Copyright (c) 2014 Christian Schulze, andinfinity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/*!
+ @header AIPNGController
+ This controller provides basic drawing to a canvas and saving the image to a png.
+ @author Christian Schulze
+ @copyright Christian Schulze, andinfinity
+ @version 0.1
+ @updated 01.03.14
+ @discussion This controller can easily be extended to provide saving to other formats as well. See: https://developer.apple.com/library/mac/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/Introduction/Introduction.html#//apple_ref/doc/uid/TP30001066
+ */
+@interface FTLOutputController : NSObject {
+	int viewHeight;
+	int viewWidth;
+	
+	CGContextRef context;
+}
+
+#pragma mark - initiating the canvas
+/*!
+ Initiate context that represents the `canvas`.
+ 
+ @param width of the context
+ @param height of the context
+ 
+ @discussion the context represents the `canvas` to draw on
+ */
+- (void) createBitmapContextWithWidth:(int)width andHeight:(int)height;
+
+#pragma mark - drawing
+
+/*!
+ Draw line on canvas starting from `start` and ending at `end`.
+ 
+ @param start of line
+ @param end of line
+ */
+- (void) drawLineFrom:(CGPoint)start to:(CGPoint)end;
+
+/*!
+ Save the image to a png at given path.
+ 
+ @param path of the png
+ @param andName:NSString* name of the png
+ */
+# pragma mark - saving the image to png
+
+- (void) saveImageToPNGWithPath:(NSString *)path andName:(NSString*)name;
+
+@end
